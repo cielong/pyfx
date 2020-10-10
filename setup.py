c3971f2b94e894ef193f4549c834227c6ca42cc2
Cielo <yutian.wu@mail.com>
2020-10-10 16:37:48 -0700

    Reslove pypi project name conflict (#6)
    * resolve project name conflict in pypi
    * set project version to 0.1.0-alpha
    * fix wrong name for MANIFEST.in
    * single source version file

--- setup.py
+++ setup.py
@@ -8,12 +8,15 @@ from setuptools import setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
+# Get the version from the VERSION file
+version = (here / 'VERSION').read_text(encoding='utf-8')
+
 # Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name="pyfx",
-    version="0.0.1",
+    name="python-fx",
+    version=version,
     author="Yutian Wu",
     author_email="yutianwu@umich.edu",
     description="A python-native fx-alike terminal JSON viewer with JSONPath Integration.",
