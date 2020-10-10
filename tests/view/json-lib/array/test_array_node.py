57b2a0a3cdaee6f006ac43399789db63ae621ae7
cielong <yutian.wu@mail.com>
2020-10-09 22:58:44 -0700

    Add more tests

--- tests/view/json-lib/array/test_array_node.py
+++ tests/view/json-lib/array/test_array_node.py
@@ -27,7 +27,18 @@ class ArrayNodeTest(unittest.TestCase):
             contents.append(widget.render((18,)).content())
             widget = widget.next_inorder()
 
+        # restart and scan from the end
+        node = ArrayNode("", data, display_key=False)
+        node.toggle_expanded()
+        widget = node.get_end_node().get_widget()
+        contents_from_end = []
+        while widget is not None:
+            contents_from_end.append(widget.render((18,)).content())
+            widget = widget.prev_inorder()
+
         texts = [[[t[2] for t in row] for row in content] for content in contents]
+        texts_from_end = [[[t[2] for t in row] for row in content] for content in contents_from_end]
+        texts_from_end.reverse()
 
         # verify
         self.assertEqual(2, len(texts))
@@ -36,6 +47,7 @@ class ArrayNodeTest(unittest.TestCase):
             [[B("]                 ")]],
         ]
         self.assertEqual(expected, texts)
+        self.assertEqual(expected, texts_from_end)
 
     def test_simple_array(self):
         """
@@ -155,5 +167,36 @@ class ArrayNodeTest(unittest.TestCase):
             [[B("      "), B("test: True  ")]],
             [[B("   "), B("}              ")]],
             [[B("]                 ")]]
+        ]
+        self.assertEqual(expected, texts)
+
+    def test_prev_order(self):
+        data = [
+            1,
+            2
+        ]
+
+        node = ArrayNode("", data, display_key=False)
+        # manual expand node
+        node.toggle_expanded()
+
+        # start from the end
+        widget = node.get_end_node().get_widget()
+
+        contents = []
+        while widget is not None:
+            contents.append(widget.render((18,)).content())
+            widget = widget.prev_inorder()
+
+        texts = [[[t[2] for t in row] for row in content] for content in contents]
+        texts.reverse()
+
+        # verify
+        self.assertEqual(4, len(texts))
+        expected = [
+            [[B("[                 ")]],
+            [[B("   "), B("1              ")]],
+            [[B("   "), B("2              ")]],
+            [[B("]                 ")]],
         ]
         self.assertEqual(expected, texts)
--- tests/view/json-lib/object/test_object_node.py
+++ tests/view/json-lib/object/test_object_node.py
@@ -27,7 +27,18 @@ class ObjectNodeTest(unittest.TestCase):
             contents.append(widget.render((18,)).content())
             widget = widget.next_inorder()
 
+        # restart and scan from the end widget
+        node = ObjectNode("", data, display_key=False)
+        node.toggle_expanded()
+        widget = node.get_end_node().get_widget()
+        contents_from_end = []
+        while widget is not None:
+            contents_from_end.append(widget.render((18,)).content())
+            widget = widget.prev_inorder()
+
         texts = [[[t[2] for t in row] for row in content] for content in contents]
+        texts_from_end = [[[t[2] for t in row] for row in content] for content in contents_from_end]
+        texts_from_end.reverse()
 
         # verify
         self.assertEqual(2, len(texts))
@@ -36,11 +47,13 @@ class ObjectNodeTest(unittest.TestCase):
             [[B("}                 ")]],
         ]
         self.assertEqual(expected, texts)
+        self.assertEqual(expected, texts_from_end)
 
     def test_simple_object(self):
         """ test rendering of a not-nested JSON object """
         data = {
-            "key": "value"
+            "k1": "v1",
+            "k2": "v2"
         }
 
         # act
@@ -59,10 +72,11 @@ class ObjectNodeTest(unittest.TestCase):
         texts = [[[t[2] for t in row] for row in content] for content in contents]
 
         # verify
-        self.assertEqual(3, len(texts))
+        self.assertEqual(4, len(texts))
         expected = [
             [[B("{                 ")]],
-            [[B("   "), B("key: value     ")]],
+            [[B("   "), B("k1: v1         ")]],
+            [[B("   "), B("k2: v2         ")]],
             [[B("}                 ")]],
         ]
         self.assertEqual(expected, texts)
@@ -138,6 +152,37 @@ class ObjectNodeTest(unittest.TestCase):
             [[B("   "), B("key: [         ")]],
             [[B("      "), B("1           ")]],
             [[B("   "), B("]              ")]],
+            [[B("}                 ")]],
+        ]
+        self.assertEqual(expected, texts)
+
+    def test_prev_order(self):
+        data = {
+            "k1": "v1",
+            "k2": "v2"
+        }
+
+        node = ObjectNode("", data, display_key=False)
+        # manual expand node
+        node.toggle_expanded()
+
+        # start from the end
+        widget = node.get_end_node().get_widget()
+
+        contents = []
+        while widget is not None:
+            contents.append(widget.render((18,)).content())
+            widget = widget.prev_inorder()
+
+        texts = [[[t[2] for t in row] for row in content] for content in contents]
+        texts.reverse()
+
+        # verify
+        self.assertEqual(4, len(texts))
+        expected = [
+            [[B("{                 ")]],
+            [[B("   "), B("k1: v1         ")]],
+            [[B("   "), B("k2: v2         ")]],
             [[B("}                 ")]],
         ]
         self.assertEqual(expected, texts)
