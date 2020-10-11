0e6268474c5a8f8687cfae0eec7849461d4c1af6
Cielo <yutian.wu@mail.com>
2020-10-11 16:46:32 -0700

    Add docs test in tox.ini and fix docs requirements(#15)

--- docs/conf.py
+++ docs/conf.py
@@ -34,13 +34,12 @@ release = (here / 'VERSION').read_text(encoding='utf-8')
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autosectionlabel',
     'sphinx.ext.autodoc',
     'sphinx_click'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -58,4 +57,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
