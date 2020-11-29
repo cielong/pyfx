4b4e3932f8e55d1288408752639f988cc3860a5c
Yutian Wu <yutian.wu@mail.com>
2020-11-29 11:56:01 -0800

    Doc

--- src/pyfx/model/common/jsonpath/__init__.py
+++ src/pyfx/model/common/jsonpath/__init__.py
@@ -1,9 +1,8 @@
 """
 A homemade JSONPath grammar using ANTLR4.
 
-The reason to build a separate grammar is to build auto-completion for JSONPath,
-and the internal lexer for :mod:`jsonpath-ng` seems not able to extend it to
-add error recovery methods.
+The reason to build a separate grammar is to build auto-completion for JSONPath
+and finding resources on building autocompletion on ANTLR4 is much easier :)
 """
 from .JSONPathParser import JSONPathParser
 from .JSONPathLexer import JSONPathLexer
