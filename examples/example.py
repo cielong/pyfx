c40504ef339d673890dd04797cdd3bf778d61cbe
Cielo <yutian.wu@mail.com>
2022-09-01 20:11:58 -0700

    Fix example code (#85)

--- examples/example.py
+++ examples/example.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python3
 
-"""This example shows how to load a JSON file and run a `pyfx` TUI. """
+"""
+This example shows how to run the `pyfx` TUI with in-memory Python variable.
+"""
 
+import json
 import pathlib
 from pyfx import PyfxApp
-from pyfx.model import DataSourceType
 
-here = pathlib.Path(__file__).parent.parent.resolve()
+# Load data from file '../data/data.json'
+parent_dir = pathlib.Path(__file__).parent.parent.resolve()
+data = json.loads(parent_dir / "data" / "data.json")
 
-if __name__ == "__main__":
-    PyfxApp().run(DataSourceType.FILE, here / "data/data.json")
+# start Pyfx
+PyfxApp(data=data).run()
