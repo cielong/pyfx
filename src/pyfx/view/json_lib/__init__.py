4b412fae9ef4ffb95e7f78ef6348f8b12e260bf6
Yutian Wu <yutian.wu@mail.com>
2022-09-07 21:08:27 -0700

    more docs

--- src/pyfx/view/json_lib/__init__.py
+++ src/pyfx/view/json_lib/__init__.py
@@ -15,20 +15,6 @@ There are two exposed classes in this module, namely :class:`.JSONListBox`,
    A separate component handles actual JSON tree iteration.
    It determines the prev and next UI widget inside :class:`.JSONListBox`.
 
-.. rubric:: Examples
-
-1. Create a :class:`JSONListBox` widget that can be used in urwid TUI.
-
-.. code-block:: python
-   :linenos:
-
-   from pyfx.json_lib import JSONListBox
-   from pyfx.json_lib import JSONListWalker
-
-   # create JSONListBox from data
-   listbox = JSONListBox(JSONListWalker(data))
-
-
 .. rubric:: Data Modeling
 
 The JSON data is loaded into memory as a tree and each node in the tree is an
@@ -43,6 +29,40 @@ are the key classes:
     and create a node.
 * :class:`.JSONNodeCreator`:
     The factory class to create a specific :class:`.JSONSimpleNode` instance.
+
+.. rubric:: Examples
+
+1. Create a :class:`JSONListBox` widget that can be used in urwid TUI.
+
+.. code-block:: python
+   :linenos:
+
+   from pyfx.json_lib import JSONListBox
+   from pyfx.json_lib import JSONListWalker
+
+   # create JSONListBox from data
+   listbox = JSONListBox(JSONListWalker(data))
+
+2. Uses a customize rendering widgets for an user-defined class.
+
+.. code-block:: python
+   :linenos:
+
+   from pyfx.json_lib import JSONListBox
+   from pyfx.json_lib import JSONListWalker
+   from pyfx.json_lib.json_node_factory import JSONNodeFactory
+   from pyfx.json_lib.json_node_creator import JSONNodeCreator
+
+   class UserNodeCreator(JSONNodeCreator):
+       def create_node(self, key, value, **kwargs):
+           return StringNode(key, str(value), **kwargs)
+
+   # create a customize JSONNodeFactory
+   node_factory = JSONNodeFactory()
+   node_factory.register(UserNodeCreator())
+
+   # create JSONListBox from data using the customize node_factory
+   listbox = JSONListBox(JSONListWalker(data, node_factory=node_factory))
 """
 from .json_listbox import JSONListBox
 from .json_listwalker import JSONListWalker
